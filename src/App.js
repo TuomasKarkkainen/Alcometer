ff4c6b71e33b69283542fd7e2defa1ee2a7e4638
Tuomas Karkkainen <tum_ppi@hotmail.com>
2021-02-02 20:22:03 +0200

    Alkometri koodi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [weight, setWeight] = useState(90);
+  const [bottles, setBottles] = useState(0);
+  const [time, setTime] = useState(0);
+ const [gender, setGender] = useState('male');
+ const [result, setResult] = useState(0);
+
+  function handleSubmit(e) {
+   e.preventDefault();
+   let liters = bottles * 0.33;
+     let grams = liters * 8 * 4.5;
+   let burning = weight /  10;
+   let left = grams - (burning * time);
+   let result = 0;
+   
+   if (gender === 'male') {
+    result = left / (weight * 0.7);
+   }
+   else {
+     result = left / (weight * 0.6);
+   }
+   setResult(result);
+ 
+ }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <>
+    <h3>Calculating alcohol blood level</h3>
+    <form onSubmit={handleSubmit}>
+  <div>
+    <div>
+ <label>Weight</label>
+ <input name="weight" type="number" step="1" value = {weight} onChange = {e => setWeight(e.target.value)} />
     </div>
+ 
+ 
+  <div>
+  <label>Bottles</label>
+  <input name="bottles" type="number" step="1" value = {bottles} onChange = {e => setBottles(e.target.value)} />
+  </div>
+ 
+  <div>
+  <label>Time</label>
+  <input name="time" type="number" step="1" value = {time} onChange = {e => setTime(e.target.value)}/>
+  </div>
+  <div>
+  <label>Gender</label>
+  <input type="radio" name="gender" value="male" defaultChecked onChange = {e => setGender(e.target.value)}/> <label>Male</label>
+  <input type="radio" name="gender" value="female" onChange = {e => setGender(e.target.value)}/>  <label>Female</label>
+  </div>
+  <div>
+    <output>{result.toFixed(1)}</output>
+  </div>
+  <button>Calculate</button>
+  </div>
+  </form>
+  </>
   );
 }
+
 
 export default App;
